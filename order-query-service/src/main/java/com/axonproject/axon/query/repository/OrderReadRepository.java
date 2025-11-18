58411e60bf0a81c789c0f2de47f303d9d6aa9dad
hamsungmin <52432072+hamsungmin@users.noreply.github.com>
2025-11-18 15:38:51 +0900

    조회 부분 JPA 관련 데이터 수정

--- order-query-service/src/main/java/com/axonproject/axon/query/repository/OrderReadRepository.java
+++ order-query-service/src/main/java/com/axonproject/axon/query/repository/OrderReadRepository.java
@@ -1,6 +1,7 @@
 package com.axonproject.axon.query.repository;
 
 import com.axonproject.axon.query.entity.OrderReadModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderReadRepository extends JpaRepository<OrderReadModel, String> {
     
